d69e310da1c0331d7fb27e8a1906ca1fe2aa4caf
kgcdream2019 <kgc.dream2019@gmail.com>
2020-10-23 02:01:04 +0800

    compiling fix for macos

--- src/libwalletqt/Wallet.cpp
+++ src/libwalletqt/Wallet.cpp
@@ -68,38 +68,38 @@ public:
 
     }
 
-    virtual void moneySpent(const std::string &txId, uint64_t amount)
+    virtual void moneySpent(const std::string &txId, uint64_t amount) override
     {
         qDebug() << __FUNCTION__;
         emit m_wallet->moneySpent(QString::fromStdString(txId), amount);
     }
 
 
-    virtual void moneyReceived(const std::string &txId, uint64_t amount)
+    virtual void moneyReceived(const std::string &txId, uint64_t amount) override
     {
         qDebug() << __FUNCTION__;
         emit m_wallet->moneyReceived(QString::fromStdString(txId), amount);
     }
 
-    virtual void unconfirmedMoneyReceived(const std::string &txId, uint64_t amount)
+    virtual void unconfirmedMoneyReceived(const std::string &txId, uint64_t amount) override
     {
         qDebug() << __FUNCTION__;
         emit m_wallet->unconfirmedMoneyReceived(QString::fromStdString(txId), amount);
     }
 
-    virtual void newBlock(uint64_t height)
+    virtual void newBlock(uint64_t height) override
     {
         // qDebug() << __FUNCTION__;
         emit m_wallet->newBlock(height, m_wallet->daemonBlockChainTargetHeight());
     }
 
-    virtual void updated()
+    virtual void updated() override
     {
         emit m_wallet->updated();
     }
 
     // called when wallet refreshed by background thread or explicitly
-    virtual void refreshed()
+    virtual void refreshed() override
     {
         qDebug() << __FUNCTION__;
         emit m_wallet->refreshed();
